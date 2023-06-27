fdded3083eb7e186ca7e2dcfecdf8f821beda357
arjunskneoastra <137884163+arjunskneoastra@users.noreply.github.com>
2023-06-27 16:02:58 +0530

    Add files via upload

--- app/admin/profile/page.js
+++ app/admin/profile/page.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ProfileRoot from "./ProfileRoot";
-export const metadata = {
-  title: "MyGenie | Profile",
-  description: "Profile",
-};
+// export const metadata = {
+//   title: "MyGenie | Profile",
+//   description: "Profile",
+// };
 
 export default function Profile() {
   return <ProfileRoot />;
